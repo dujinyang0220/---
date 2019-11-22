c653da6d8e76fcef6357411dc2af8edf857dae54
dujinyang <645869563@qq.com>
2019-11-22 23:11:24 +0800

    爬虫工具

---
+++ utils/configwusong.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+# 搜索关键字
+KEYWORD = ''
+
+# URL
+URL = 'https://www.itslaw.com/search?searchMode=judgements&sortType=1&conditions=searchWord%2B%E6%B3%95%E5%BE%8B%2B1%2B%E6%B3%95%E5%BE%8B&searchView=text"'
+
+
+# 数据库配置
+MONGO_URL = 'localhost'
+MONGO_DB = 'wusong'
+MONGO_COLLECTION = 'mrc'
+
+USERNAME='13667272850'
+PASSWORD='mssjwow123'
+# 加载延时
+TIMEOUT = 30
+
+# 最大页数
+MAX_PAGE = 10
